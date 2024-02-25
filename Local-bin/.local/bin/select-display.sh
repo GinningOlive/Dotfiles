7be026c96256897a8a36c2ca10a5506db465f5f3
GinningOlive <121423088+GinningOlive@users.noreply.github.com>
2024-02-25 01:38:31 -0800

    Update select-display.sh

--- Local-bin/.local/bin/select-display.sh
+++ Local-bin/.local/bin/select-display.sh
@@ -5,7 +5,7 @@ choices="Extend\nDuplicate\neDP-1\nHDMI-1"
 chosen=$(echo -e "$choices" | rofi -dmenu)
 
 case "$chosen" in
-	Extend) xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal ;;
+	Extend) xrandr --output eDP-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal && ~/.fehbg ;;
 	Duplicate) xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal ;;
 	eDP-1) xrandr --output eDP-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-1 --off ;;
 	HDMI-1) xrandr --output eDP-1 --off --output HDMI-1 --mode 1920x1080 --pos 0x0 --rotate normal ;;
