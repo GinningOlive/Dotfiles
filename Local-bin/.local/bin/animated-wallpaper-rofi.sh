313b62c698545978ba3bc8038d093d29755129bb
GinningOlive <121423088+GinningOlive@users.noreply.github.com>
2024-07-10 18:23:51 -0700

    Update animated-wallpaper-rofi.sh

--- Local-bin/.local/bin/animated-wallpaper-rofi.sh
+++ Local-bin/.local/bin/animated-wallpaper-rofi.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-killall bash ; back4.sh 0.01 ~/Pictures/gifs/$(ls ~/Pictures/gifs/ | rofi -dmenu)
+# killall bash ; back4.sh 0.01 ~/Pictures/gifs/$(ls ~/Pictures/gifs/ | rofi -dmenu)
+
+killall bash ; gif-wallpaper.sh 0.01 ~/Pictures/gifs/$(ls ~/Pictures/gifs/ | rofi -dmenu)
