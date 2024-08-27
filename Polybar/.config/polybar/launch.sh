dbf86437fd39f2321a935fd97a9749d4a66878c0
GinningOlive <121423088+GinningOlive@users.noreply.github.com>
2024-08-26 17:56:01 -0700

    Update launch.sh

--- Polybar/.config/polybar/launch.sh
+++ Polybar/.config/polybar/launch.sh
@@ -3,7 +3,8 @@
 source ~/.cache/wal/colors.sh
 export color0_alpha="#bf${color0/'#'}"
 
-killall polybar
+killall polybar &
+killall .polybar-wrapped &
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 polybar main -c ~/.config/polybar/config.ini
