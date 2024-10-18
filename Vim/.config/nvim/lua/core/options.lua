b048e95fe824c7dd064b061efe470f3fdbed6006
GinningOlive <121423088+GinningOlive@users.noreply.github.com>
2024-10-18 23:23:15 +0000

    Create options.lua

---
+++ Vim/.config/nvim/lua/core/options.lua
@@ -0,0 +1,30 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+-- vim.g.maplocalleader = " "
+
+-- vim.opt.backspace = '2'
+-- vim.opt.showcmd = true
+-- vim.opt.laststatus = 2
+-- vim.opt.autowrite = true
+-- vim.opt.cursorline = true
+-- vim.opt.autoread = true
+
+-- use spaces for tabs
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 4
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt_local.spelllang = 'en_us'
+vim.opt_local.spell = false
+-- vim.opt_local.spell = true
+-- vim.opt_local.spell = not vim.opt_local.spell:get()
+
+-- vim.opt.clipboard = "unnamedplus"
