f4169749742ad32c13e935212f41f75134cf8e37
GinningOlive <121423088+GinningOlive@users.noreply.github.com>
2024-11-27 00:20:19 +0000

    Update launch.sh

--- Polybar/.config/polybar/launch.sh
+++ Polybar/.config/polybar/launch.sh
@@ -7,4 +7,8 @@ killall polybar &
 killall .polybar-wrapped &
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar main -c ~/.config/polybar/config.ini
+# polybar main -c ~/.config/polybar/config.ini
+
+polybar top-l -c ~/.config/polybar/config.ini &
+sleep 1
+polybar top-r -c ~/.config/polybar/config.ini
