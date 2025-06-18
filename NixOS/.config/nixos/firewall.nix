e41ce55e3124dd0ac59ff9b7ffabbb4849f70f7a
GinningOlive <121423088+GinningOlive@users.noreply.github.com>
2025-06-18 14:20:03 -0700

    Update firewall.nix

--- NixOS/.config/nixos/firewall.nix
+++ NixOS/.config/nixos/firewall.nix
@@ -3,7 +3,9 @@
 {
   networking.firewall = {
     enable = true;
-    allowedTCPPorts = [ 7000 7001 7100 ];
+    allowedTCPPorts = [ 7000 7001 7100
+    53317 # LocalSend
+     ];
     allowedUDPPorts = [ 5353 6000 6001 7011 ];
     allowedTCPPortRanges = [
       { from = 1714; to = 1764; } # KDE Connect
@@ -13,3 +15,6 @@
     ];
   };
 }
+
+# LocalShare
+# https://www.reddit.com/r/NixOS/comments/18kq1en/comment/kdvddt7/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
